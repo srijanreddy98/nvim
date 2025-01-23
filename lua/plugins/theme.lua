a81020e4dab72a93bd789bb67750336251b4f3bd
Srijan Reddy Vasa <redvasa@dev-dsk-redvasa-1a-0932ac88.eu-west-1.amazon.com>
2025-01-23 06:44:47 +0000

    Init config from Lazy and add OSC52 clipboard, typescript tools and use rose-pine theme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "main",
+      enable = {
+        terminal = true,
+      },
+
+      styles = {
+        transparency = true,
+      },
+    }
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine"
+    },
+  }
+}
